8ad7b1ed1b85f8fc8c078f7cb4463784f26c919f
Rishika92 <rishikaa.7229@gmail.com>
2026-07-04 00:06:33 +0530

    implemented recycle bin and permenant delete

--- server/src/controllers/file.controller.js
+++ server/src/controllers/file.controller.js
@@ -315,3 +315,97 @@ export const moveFile = async (req, res) => {
     });
   }
 };
+export const getTrashFiles = async (req, res) => {
+  try {
+    const files = await File.find({
+      owner: req.user._id,
+      isDeleted: true,
+    }).sort({ updatedAt: -1 });
+
+    return res.status(200).json({
+      success: true,
+      count: files.length,
+      files,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+export const restoreFile = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const file = await File.findOne({
+      _id: id,
+      owner: req.user._id,
+      isDeleted: true,
+    });
+
+    if (!file) {
+      return res.status(404).json({
+        success: false,
+        message: "File not found",
+      });
+    }
+
+    file.isDeleted = false;
+    await file.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "File restored successfully",
+      file,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+export const permanentDeleteFile = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const file = await File.findOne({
+      _id: id,
+      owner: req.user._id,
+      isDeleted: true,
+    });
+
+    if (!file) {
+      return res.status(404).json({
+        success: false,
+        message: "File not found",
+      });
+    }
+
+    await cloudinary.uploader.destroy(file.publicId, {
+      resource_type: "auto",
+    });
+
+    await File.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      success: true,
+      message: "File permanently deleted",
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};  
--- server/src/routes/file.routes.js
+++ server/src/routes/file.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import protect from "../middleware/auth.middleware.js";
 import upload from "../middleware/upload.middleware.js";
-import { uploadFile ,getFiles,deleteFile,searchFiles,getFileById,renameFile,moveFile} from "../controllers/file.controller.js";
+import { uploadFile ,getFiles,deleteFile,searchFiles,getFileById,renameFile,moveFile,getTrashFiles,
+restoreFile,
+permanentDeleteFile} from "../controllers/file.controller.js";
 
 const router = express.Router();
 
@@ -17,4 +19,7 @@ router.get("/search", protect, searchFiles);
 router.get("/:id", protect, getFileById);
 router.patch("/:id/rename", protect, renameFile);
 router.patch("/:id/move", protect, moveFile);
-export default router;
+router.get("/trash", protect, getTrashFiles);
+router.patch("/:id/restore", protect, restoreFile);
+router.delete("/:id/permanent", protect, permanentDeleteFile);
+export default router;  
